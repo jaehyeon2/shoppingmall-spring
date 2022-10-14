3f9bb5766f872d2bc3c47f7c36732e1f59f952a4
jaehyeon2 <wol5971@gmail.com>
2022-10-14 13:41:09 +0900

    Initial commit

---
+++ src/test/java/com/example/shoppingmallspring/ShoppingmallSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shoppingmallspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingmallSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
