6a35567a2c0a3304f1bc651353a8460ae9201525
jaehyeon2 <wol5971@gmail.com>
2022-10-18 16:55:12 +0900

    MemberService Test

---
+++ src/test/java/com/example/shoppingmallspring/service/MemberServiceTest.java
@@ -0,0 +1,45 @@
+package com.example.shoppingmallspring.service;
+
+import com.example.shoppingmallspring.domain.members.Member;
+import com.example.shoppingmallspring.dto.MemberFormDto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@TestPropertySource(properties = {"spring.config,location=classpath:application-test.yml"})
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    public Member createMember(){
+        MemberFormDto memberFormDto = MemberFormDto.builder()
+                .email("test@email.com")
+                .name("test")
+                .address("제주도 서귀포시")
+                .password("1234")
+                .build();
+        return Member.createMember(memberFormDto, passwordEncoder);
+    }
+
+    @Test
+    @DisplayName("회원가입 테스트")
+    public void saveMemberTest(){
+        Member member = createMember();
+        Member savedMember = memberService.saveMember(member);
+
+        assertEquals(member.getEmail(), savedMember.getEmail());
+    }
+
+}
