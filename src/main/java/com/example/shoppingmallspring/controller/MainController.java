7fd07fc5c6ddee445bacfe5645b264e8d1195f3f
jaehyeon2 <wol5971@gmail.com>
2022-10-19 19:42:56 +0900

    item code & login code

---
+++ src/main/java/com/example/shoppingmallspring/controller/MainController.java
@@ -0,0 +1,24 @@
+package com.example.shoppingmallspring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String main(){
+        return "main";
+    }
+
+    @GetMapping("/login")
+    public String loginMember(){
+        return "/member/memberLoginForm";
+    }
+
+    @GetMapping("/login/error")
+    public String loginError(Model model){
+        model.addAttribute("loginErrorMsg", "아이디 또는 비밀번호를 확인하세요.");
+        return "/member/memberLoginForm";
+    }
+}
